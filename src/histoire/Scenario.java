12709b5c105706f2854f667dfc79cfaf8121fd9d
ComeGuyard <114427105+ComeGuyard@users.noreply.github.com>
2022-10-17 17:35:14 +0200

    moitié TP2

--- src/histoire/Scenario.java
+++ src/histoire/Scenario.java
@@ -9,26 +9,26 @@ public class Scenario {
 
 	public static void main(String[] args) {
 		
-		Gaulois asterix = new Gaulois("Astérix",8);
-		Romain minus = new Romain("Minus",6);
-		Druide panoramix = new Druide("Panoramix",5,10);
-		Gaulois obelix = new Gaulois("Obélix",100);
-		
-		
-		panoramix.parler("Je vais aller préparer une petite potion.");
-		panoramix.preparerPotion();
-		
-		panoramix.booster(obelix);
-		obelix.parler("Par Bélénos ce n'est pas juste!");
-		
-		asterix.boirePotion(panoramix.forcePotion);
-		asterix.parler("Bonjour");
-		
-		minus.parler("UN GAU.. UN GAUGAU...");
-		
-		asterix.frapper(minus);
-		asterix.frapper(minus);
-		asterix.frapper(minus);
+//		Gaulois asterix = new Gaulois("Astérix",8);
+//		Romain minus = new Romain("Minus",6);
+//		Druide panoramix = new Druide("Panoramix",5,10);
+//		Gaulois obelix = new Gaulois("Obélix",100);
+//		
+//		
+//		panoramix.parler("Je vais aller préparer une petite potion.");
+//		panoramix.preparerPotion();
+//		
+//		panoramix.booster(obelix);
+//		obelix.parler("Par Bélénos ce n'est pas juste!");
+//		
+//		asterix.boirePotion(panoramix.forcePotion);
+//		asterix.parler("Bonjour");
+//		
+//		minus.parler("UN GAU.. UN GAUGAU...");
+//		
+//		asterix.frapper(minus);
+//		asterix.frapper(minus);
+//		asterix.frapper(minus);
 	}
 
 }
--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -13,7 +13,7 @@ public class Village {
 	 
 	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
-		this.villageois = Gaulois[nbVillageoisMaximum] ;
+		this.villageois = new Gaulois[nbVillageoisMaximum] ;
 	}
 	
 	public void setChef(Chef chef) {
@@ -33,11 +33,29 @@ public class Village {
 		return villageois[numgaulois] ;
 	}
 	
-	public static void main() {
+	
+	public void afficherVillageois() {
+		System.out.println("Dans le village du chef " + chef.getNom() + "vivent les légendaires gaulois :");
+		for(int i = 0 ; i < nbVillageois ; i++) {
+			System.out.println("- " + villageois[i].getNom());
+		}
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
 		
 		Village village = new Village("Village des Irréductibles",30) ;
-		Gaulois gaulois = village.trouverHabitant(30);
+		//Gaulois gaulois = village.trouverHabitant(30);
+		Chef abraracourcix = new Chef("Abraracourcix",6,0,village) ;
+		village.setChef(abraracourcix);
 		
+		Gaulois asterix = new Gaulois("Astérix",8) ; 
+		village.ajouterHabitant(asterix);
+//		Gaulois gaulois = village.trouverHabitant(1);
+//		System.out.println(gaulois);
+		village.afficherVillageois() ;
 	}
 	
 	}
